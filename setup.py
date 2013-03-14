006e15186035c5393858d103cec1288a3f97f921
Philip Horger <campadrenalin@gmail.com>
2013-03-14 14:02:03 -0700

    First attempt at installability via pip

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
 setup(name='congress',
       version='0.1.0',
-      cmdclass = {"build_ext": build_ext},
       description='Kademlia-like DHT Implementation',
       author='Derek Arnold',
       author_email='derek@brainindustries.com',
       url='http://lysol.github.com/congress',
       packages=['congress'],
       package_dir={'congress': 'src/congress'},
-      ext_modules=[Extension("_congress", ["src/congress/_congress.pyx"])],
+      ext_modules=[Extension("_congress", ["src/congress/_congress.c"])],
       license='BSD',
       classifiers=[
         "Development Status :: 3 - Alpha",
@@ -29,5 +27,6 @@ setup(name='congress',
       requires=(
         'pyev',
         'dogfood'
-        )
+        ),
+      install_requires = ('pyev',),
      )
